acd550c1439ee1e4866574f0d49d33a403b8c306
Rishi Bachhuka <rishibachhuka5678@gmail.com>
2024-01-29 02:21:40 +0530

    Made some balance changes

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-var v = 0;
-var u=0;
-var y = 100;
-// var g =9.8;
-// var e=0.8;
-// var d=0;
+var v = 0, u = 0, y = 100;
 var g, e, d;
 var z = 0;
-var end=446;
-var gg,ee,dd;
+var end = 446;
+var gg, ee, dd;
+
 $(".restart").click(function () {
     z = 0;
-    y=100;
-    v=0;
-    $("#grr").attr("value","9.8");
-    $("#cor").attr("value","0.8");
-    $("#add").attr("value","0.0");
-    $(".ball").css("top",y);
+    y = 100;
+    v = 0;
+    $("#grr").attr("value", "9.8");
+    $("#cor").attr("value", "0.8");
+    $("#add").attr("value", "0.0");
+    $(".ball").css("top", y);
 });
 
 $(".pause").click(function () {
@@ -23,55 +19,41 @@ $(".pause").click(function () {
 });
 
 $(".play").click(function () {
-    //alert("hell");
     z = 1;
 });
 
-$(document).ready(function(){
-    // alert("ram");
-    myVar = setInterval("calculate()", 1000/60);
+$(document).ready(function () {
+    myVar = setInterval("calculate()", 1);
 });
 
-function calculate(){
-    
-    if(z==1){
-        //console.log("hi");
-        var j=y;
-        g=document.getElementById("grr").value; 
-        e=document.getElementById("cor").value; 
-        d=document.getElementById("add").value;
-        // console.log("v="+v);
-        // if(v<10.0&&v>-10){
-        // console.log(450-(j-100)-100);}
-        gg=parseFloat(g);
-        gg=gg*1;
-        ee=parseFloat(e);
-        dd=parseFloat(d);
-        if(j>=end){
-            v=e*(-v);
-            j=end-(j-end);
-        }
-        else{
+function calculate() {
 
+    if (z == 1) {
+        var j = y;
+        g = document.getElementById("grr").value;
+        e = document.getElementById("cor").value;
+        d = document.getElementById("add").value;
+        gg = parseFloat(g);
+        ee = parseFloat(e);
+        dd = parseFloat(d);
+        gg = gg * 1;
+        if (j >= end) {
+            v = e * (-v);
+            j = end - (j - end);
         }
-        if(v>=0){
-            v+=gg;
-            v-=v*dd*0.001;
+        if (v >= 0) {
+            v += gg;
+            v -= v * v * dd * 0.000001;
         }
-        else{
-            v+=gg;
-            v-=v*dd*0.001;
+        else {
+            v += gg;
+            v += v * v * dd * 0.000001;
         }
-       
-        j=j+v*0.005;
-        
-        
-
-        y=j;
-        // console.log(y);
-        $(".ball").css("top",y+"px");
-
+        j = j + v * 0.005;
+        y = j;
+        if (j <= end)
+            $(".ball").css("top", y + "px");
+        else
+            $(".ball").css("top", end + "px");
     }
-
-
 }
